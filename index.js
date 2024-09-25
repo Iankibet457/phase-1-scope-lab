4b26bfd107831307b3d65b51a7ac2dfcb918e288
ian mosonik <ian.mosonik@student.moringaschool.com>
2024-09-25 16:31:05 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution in this file!
+var customerName = "bob"
+// console.log(customerName)
+upperCaseCustomerName(customerName)
+
+function upperCaseCustomerName(cn){
+    var results = cn
+    // var results =cn.toUpperCase()
+    // return results
+    
+    var r =(results.toUpperCase())
+    console.log(r)
+    return r
+    // return (results.toUpperCase())
+    
+
+
+}
+setBestCustomer()
+
+function setBestCustomer(){
+
+    var bestCustomer = "not bob"
+    return bestCustomer
+}
+// console.log(setBestCustomer())
+// console.log(bestCustomer)
+
+function overwriteBestCustomer(bestCustomer){
+    return bestCustomer = "maybe bob"
+
+}
+overwriteBestCustomer()
+
+const leastFavoriteCustomer = "cow"
+
+function changeLeastFavoriteCustomer(x){
+    x = "chicken"
+    return x
+
+}
+
+changeLeastFavoriteCustomer(leastFavoriteCustomer)
